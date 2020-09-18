bcb840a8b650e825d7e5fafc6c9a8449d592a347
unisannino <unisannino@gmail.com>
2020-09-18 20:20:21 +0900

    スライドを進行するためのシステムを構築

---
+++ Assets/Scripts/Slide.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Slide : MonoBehaviour
+{
+    [SerializeField]
+    UnityEvent _resetEvent;
+    [SerializeField]
+    UnityEvent _finishEvent;
+    [SerializeField]
+    UnityEvent[] _events;
+    [SerializeField]
+    byte _currentEventId;
+
+    Canvas _canvas;
+    internal bool IsComplete => _currentEventId >= _events.Length;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _canvas = GetComponent<Canvas>();
+    }
+
+    internal void ResetSlide()
+    {
+        _canvas.enabled = true;
+        _currentEventId = 0;
+        _resetEvent.Invoke();
+    }
+
+    internal void ExecuteNextEvent()
+    {
+        _events[_currentEventId].Invoke();
+        _currentEventId++;
+    }
+
+    internal void FinishSlide()
+    {
+        _canvas.enabled = false;
+        _finishEvent.Invoke();
+    }
+}
---
+++ Assets/Scripts/SlideManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class SlideManager : MonoBehaviour
+{
+    [SerializeField]
+    Slide[] _slides;
+    [SerializeField]
+    int _currentSlideId;
+
+    Slide CurrentSlide => _slides[_currentSlideId];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _slides = GetComponentsInChildren<Slide>();
+
+        CurrentSlide.ResetSlide();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsPressedButtonNext())
+        {
+            if (CurrentSlide.IsComplete && _currentSlideId < _slides.Length)
+            {
+                CurrentSlide.FinishSlide();
+                _currentSlideId++;
+                CurrentSlide.ResetSlide();
+            }
+            else
+            {
+                CurrentSlide.ExecuteNextEvent();
+            }
+        }
+        else if (IsPressedButtonBack() && _currentSlideId > 0)
+        {
+            CurrentSlide.FinishSlide();
+            _currentSlideId--;
+            CurrentSlide.ResetSlide();
+        }
+    }
+
+    bool IsPressedButtonNext()
+    {
+        return Input.GetMouseButtonDown((byte)MouseButton.LeftMouse) && !IsPressedButtonBack();
+    }
+    bool IsPressedButtonBack()
+    {
+        return Input.GetMouseButtonDown((byte)MouseButton.RightMouse) && !IsPressedButtonNext();
+    }
+}
